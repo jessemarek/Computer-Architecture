ac0ab7adf34b17b3197bad50d806a9233d80960e
unknown <dethgrenade@gmail.com>
2020-08-17 12:56:58 -0700

    WiP day 1 - added ram_read() and ram_write()

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -65,3 +65,9 @@ class CPU:
     def run(self):
         """Run the CPU."""
         pass
+
+    def ram_read(self, mar):
+        return self.ram[mar]
+
+    def ram_write(self, mdr, mar):
+        self.ram[mar] = mdr
